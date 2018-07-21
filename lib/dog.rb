6c556b750fa190b45d0e475490aee29e07117545
Lorena <lorena.giddens@outlook.com>
2018-07-21 14:05:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,4 +53,17 @@ class Dog
     Dog.new(id: result[0], name: result[1], breed: result[2])
   end
 
+  def find_or_create_by(name:, breed:)
+    # query the database
+    song = DB[:conn].execute("SELECT dogs.* FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty
+      # reinstantiate an existing Dog object
+      dog_data = dog[0]
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
 end
